41d1d9af8c32557e27f7d6ebd7803299fa7146bb
PockLost <pocktuc@gmail.com>
2023-03-11 16:52:31 +0800

    首次提交

---
+++ config.js
@@ -0,0 +1,29 @@
+// 配置
+window.Config = {
+
+  // 顯示標題
+  SiteName: '網站狀態',
+
+  // UptimeRobot Api Keys
+  ApiKeys: [
+	'm793851644-93a3bdc87a3193d51dd5e9bd',
+	'm793851655-cb85a0e149c7fa68bf955a0a',
+	'm793851659-12cba59046f24f3b6ee5bfdd',
+	'm793851658-851d7e5e37c2e8ad49285584',
+	'm792334718-60e92db4a4782ff2f6cf0c1b',
+  ],
+
+    // 日誌天數
+  CountDays: 30,
+
+  // 是否顯示檢測站點的連結
+  ShowLink: true,
+
+  // 導航欄菜單
+  Navi: [
+    {
+      text: '活動首頁',
+      url: 'https://nesuirys.com/'
+    },
+  ],
+};
